37746d42486745cbaa3c6f047aceb9e44252b5dc
Pascal Mainini <pascal@mainini.ch>
2016-12-04 22:20:30 +0100

    initially added

---
+++ src/main/java/ch/mainini/famodulus/server/CORSResponseFilter.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016 Pascal Mainini
+ * Licensed under MIT license, see included file LICENSE or
+ * http://opensource.org/licenses/MIT
+ */
+package ch.mainini.famodulus.server;
+
+import java.util.logging.Logger;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * A filter which adds CORS-headers to all responses.
+ * @author Pascal Mainini
+ */
+@Provider
+public class CORSResponseFilter implements ContainerResponseFilter {
+
+//////////////////////////////////////// Constants
+
+    private static final Logger LOG = Logger.getLogger(CORSResponseFilter.class.getName());
+
+    /**
+     * Allow requests for any origin
+     * @todo make configurable
+     */
+    private static final String ALLOW_ORIGIN = "*";
+
+
+//////////////////////////////////////// Constructors
+
+    /**
+     * Default constructor, used for debug-logging
+     */
+    public CORSResponseFilter() {
+        LOG.finest("Filter instantiated!");
+    }
+
+
+//////////////////////////////////////// Methods
+
+    /**
+     * Filter-method which adds all relevant CORS headers to outgoing responses.
+     * @param ctx the context of the request
+     * @param res the context of the response
+     */
+    @Override
+    public void filter(ContainerRequestContext ctx, ContainerResponseContext res) {
+        MultivaluedMap<String, Object> headers = res.getHeaders();
+        headers.add("Access-Control-Allow-Origin", ALLOW_ORIGIN);
+        headers.add("Access-Control-Allow-Methods", "GET, POST");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    }
+}
